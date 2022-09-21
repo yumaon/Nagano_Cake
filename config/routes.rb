ac20cb04eced0ef7aa18ab3192ed2b72b00e710f
yumaon <110388364+yumaon@users.noreply.github.com>
2022-09-21 11:22:49 +0000

    [Update]ルーティングの記述の編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,53 +1,46 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
+  root to: "public/homes#top"
+  get 'about' => 'public/homes#about'
+ 
+  get 'customers/my_page' => 'public/customers#show'
+  get 'customers/information/edit' => 'public/customers#edit'
+  patch 'customers/information' => 'public/customers#update'
+  get 'customers/unsubscribe' => 'public/customers#unsubscribe'
+  patch 'customers/hide' => 'public/customers#hide'
+  
+
+  scope module: :public do
+    
+    resources :items, only:[:index, :show]
+    
+    resources :cart_items, only:[:index, :update, :destroy, :create,]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    
+    resources :orders, only:[:new, :create, :index, :show]
+    post 'orders/confirm'
+    get 'orders/complete' 
+    
+    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+    
   end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/unsubscribe'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+
   namespace :admin do
-    get 'orders/show'
+    
+    get '/' => 'homes#top'
+    
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    
+    resources :genres, only:[:index, :create, :edit, :update]
+    
+    resources :customers, only:[:index, :show, :edit, :update]
+    
+    resources :orders, only:[:show, :update] do
+      resources :order_details, only:[:update]
+    end
+    
   end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
+  
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }

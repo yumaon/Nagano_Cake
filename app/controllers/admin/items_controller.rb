412f7cf79e2c5b08ef1058f024c10f0e97609b2f
yumaon <110388364+yumaon@users.noreply.github.com>
2022-09-22 01:26:40 +0000

    [Add]商品新規登録画面の実装(未完)

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,11 +3,24 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price)
   end
 end

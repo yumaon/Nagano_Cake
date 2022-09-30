86c7e50c371da553f30aa261df32306c7c5dedfe
yumaon <110388364+yumaon@users.noreply.github.com>
2022-09-30 01:52:13 +0000

    [Add]顧客 登録情報編集画面の実装 「退会する」ボタンは未実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,8 +5,21 @@ before_action :authenticate_customer!
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    customer = current_customer
+    customer.update(customer_params)
+    redirect_to customers_my_page_path
   end
 
   def unsubscribe
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number,:email)
   end
 end

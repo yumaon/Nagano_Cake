232a7d52dc2ce3a32bddb1df1592570ec4d10cbc
yumaon <110388364+yumaon@users.noreply.github.com>
2022-10-01 05:12:47 +0000

    [Add]顧客 配送先登録・一覧画面の実装

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,29 @@
 class Public::AddressesController < ApplicationController
   def index
+    @customer = current_customer
+    @address = Address.new
+    @addresses = @customer.addresses
+  end
+
+  def create
+    address = current_customer.addresses.new(address_params)
+    address.save
+    redirect_to addresses_path
   end
 
   def edit
   end
+
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+
 end

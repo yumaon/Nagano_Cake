2e4fec70545453311b088dd947e0310fa9034380
yumaon <110388364+yumaon@users.noreply.github.com>
2022-10-02 10:52:49 +0000

    [Add]商品詳細画面の実装(カート機能)

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,25 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items.all
+  end
+
+  def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    # 商品を追加した際、既に同じ商品がカート内に存在する場合(数の追加)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    # カート内に同じ商品が存在しない、通常の保存処理。
+    elsif @cart_item.save
+      redirect_to cart_items_path
+    end
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end

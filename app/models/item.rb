c97484645ea29e93bbb533bb2ae40da4a4d589c2
yumaon <110388364+yumaon@users.noreply.github.com>
2022-10-03 00:25:42 +0000

    [Update]金額が表示される記述を一部変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,9 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
+  
+  def with_tax_price
+     (price * 1.1).floor
+  end
+  
 end

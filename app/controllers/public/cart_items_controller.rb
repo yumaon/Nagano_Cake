f26ef9ef5ca270974ab9e4aba794fff7f9809cdd
yumaon <110388364+yumaon@users.noreply.github.com>
2022-10-03 02:01:02 +0000

    [Add]カート画面の実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -17,6 +17,12 @@ class Public::CartItemsController < ApplicationController
     end
   end
 
+  def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path
+  end
+
   def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy

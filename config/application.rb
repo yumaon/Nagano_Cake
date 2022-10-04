763f5fbc54e4811aa35351df07cacb54260321f9
yumaon <110388364+yumaon@users.noreply.github.com>
2022-10-04 18:14:46 +0000

    [Add]enum　order.rbの記述の編集、ymlファイルの作成

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module NaganoCake
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
     #

d064946805df0fcda046a63f8565283838119430
yumaon <110388364+yumaon@users.noreply.github.com>
2022-10-05 13:52:53 +0000

    [Update]order.rbとja.ymlの記述を一部変更(enum)

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,5 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
   enum payment_method: { credit_card: 0, transfer: 1 }
-  enum status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }
+  enum status: { payment_waiting: 0, payment_confirmation: 1, in_production: 2, preparing_delivery: 3, delivered: 4 }
 end

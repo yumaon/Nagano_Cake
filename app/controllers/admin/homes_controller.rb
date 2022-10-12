0c78b21d0c6b48c5268ed93af295d07434385158
yumaon <110388364+yumaon@users.noreply.github.com>
2022-10-12 07:02:34 +0000

    [Add]管理者側　顧客ごとの注文履歴一覧ページを実装

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,11 @@
 class Admin::HomesController < ApplicationController
   def top
-    @orders = Order.all
+    if params[:email].present?
+      @orders = Customer.find(params[:email]).orders
+      @list_name = Customer.find(params[:email]).name_display + "さんの注文履歴"
+    else
+      @orders = Order.all
+      @list_name = "注文履歴一覧"
+    end
   end
 end

da56162376fe84d6745c95ec5d2e1ed989b35db3
yumaon <110388364+yumaon@users.noreply.github.com>
2022-10-12 07:23:02 +0000

    [Update]管理者側　顧客ごとの注文履歴一覧 controllerとview内の記述を一部修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,8 +1,8 @@
 class Admin::HomesController < ApplicationController
   def top
-    if params[:email].present?
-      @orders = Customer.find(params[:email]).orders
-      @list_name = Customer.find(params[:email]).name_display + "さんの注文履歴"
+    if params[:name].present?
+      @orders = Customer.find(params[:name]).orders
+      @list_name = Customer.find(params[:name]).name_display + "さんの注文履歴"
     else
       @orders = Order.all
       @list_name = "注文履歴一覧"

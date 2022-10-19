b9995b522a906407cb215c1db1b6a0777919a232
yumaon <110388364+yumaon@users.noreply.github.com>
2022-10-19 04:55:36 +0000

    [Fix][Add]管理者側　注文履歴一覧画面のレイアウト編集・ページネーションの設定

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,10 +1,10 @@
 class Admin::HomesController < ApplicationController
   def top
     if params[:name].present?
-      @orders = Customer.find(params[:name]).orders
+      @orders = Customer.find(params[:name]).orders.page(params[:page]).per(10)
       @list_name = Customer.find(params[:name]).name_display + "さんの注文履歴"
     else
-      @orders = Order.all
+      @orders = Order.page(params[:page]).per(10)
       @list_name = "注文履歴一覧"
     end
   end

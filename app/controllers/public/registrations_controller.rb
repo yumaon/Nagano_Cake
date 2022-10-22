af9eff74b83625f5f5b7e2ef4729c46dd095843b
yumaon <110388364+yumaon@users.noreply.github.com>
2022-10-22 23:58:10 +0000

    [Update]顧客側　sign_up後の遷移先を設定

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -5,6 +5,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    customers_my_page_path(current_customer.id)
+  end
+
   protected
 
   def configure_permitted_parameters

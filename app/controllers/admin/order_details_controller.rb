33e2b953132fca2b8c20ce0f1e9714e3380ea146
yumaon <110388364+yumaon@users.noreply.github.com>
2022-10-23 05:29:16 +0000

    [Update]注文ステータスと製作ステータスの連動を実装

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,10 +3,17 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     @order = Order.find(params[:order_id])
-    @order_details = OrderDetail.find(params[:id])
-    if @order_details.update(order_detail_params)
-      @order.update(status: 2) if @order_details.making_status == "in_production"
-      @order.update(status: 3) if @order_details.making_status == "production_complete"
+    @order_detail = OrderDetail.find(params[:id])
+    @order_details = @order.order_details.all
+    is_updated = true
+    if @order_detail.update(order_detail_params)
+      @order.update(status: 2) if @order_detail.making_status == "in_production"
+      @order_details.each do |order_detail|
+        if order_detail.making_status != "production_complete"
+          is_updated = false
+        end
+      end
+      @order.update(status: 3) if is_updated
     end
     redirect_to admin_order_path(@order)
   end
